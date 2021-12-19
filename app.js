6e40cdc57d31053abc7763346e175d1ad11f72e0
Steven Cruz <cruz801@hotmail.com>
2021-12-19 12:35:05 -0700

    captured

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
 
 
-fs.writeFile('./index.html', generatePage(name,github), err => {
-    if (err) throw err;
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+])
+.then(answers => console.log(answers));
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-    console.log('Portfolio complete! Check out index.html to see the output!')
-});
+// const pageHTML = generatePage(name, github);
+
+// const [name, github] = profileDataArgs;
+
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// });
 
 
 
